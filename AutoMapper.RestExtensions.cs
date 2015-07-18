177da36555c28067c5035e3c7a8ac804740d3b67
andy <andy@andydote.co.uk>
2015-07-18 13:35:29 +0100

    the whole project boils down to these 10~ lines

---
+++ AutoMapper.RestExtensions.cs
@@ -0,0 +1,13 @@
+public static class Extensions
+{
+	public static IMappingExpression<TSource, TDestination> Link<TSource, TDestination>(this IMappingExpression<TSource, TDestination> mapping, string key, Func<TSource, string> createLink)
+		where TDestination : IRestLinked
+	{
+		return mapping.AfterMap((u, r) => r.Links[key] = createLink(u));
+	}
+}
+
+public interface IRestLinked
+{
+	Dictionary<string, string> Links { get; set; }
+}
