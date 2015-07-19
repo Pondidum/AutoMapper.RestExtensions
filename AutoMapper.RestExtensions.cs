1b86f262404e8eac62e703821b8250021afe870e
andy <andy@andydote.co.uk>
2015-07-19 16:35:17 +0100

    extensions: add namesspace, usings

--- AutoMapper.RestExtensions.cs
+++ AutoMapper.RestExtensions.cs
@@ -1,13 +1,19 @@
-public static class Extensions
-{
-	public static IMappingExpression<TSource, TDestination> Link<TSource, TDestination>(this IMappingExpression<TSource, TDestination> mapping, string key, Func<TSource, string> createLink)
-		where TDestination : IRestLinked
-	{
-		return mapping.AfterMap((u, r) => r.Links[key] = createLink(u));
-	}
-}
+using System;
+using System.Collections.Generic;
 
-public interface IRestLinked
+namespace AutoMapper.Rest
 {
-	Dictionary<string, string> Links { get; set; }
+	public static class Extensions
+	{
+		public static IMappingExpression<TSource, TDestination> Link<TSource, TDestination>(this IMappingExpression<TSource, TDestination> mapping, string key, Func<TSource, string> createLink)
+			where TDestination : IRestLinked
+		{
+			return mapping.AfterMap((u, r) => r.Links[key] = createLink(u));
+		}
+	}
+
+	public interface IRestLinked
+	{
+		Dictionary<string, string> Links { get; set; }
+	}
 }
