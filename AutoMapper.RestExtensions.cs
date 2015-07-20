39cd20467b471c3a6a9772102f50b2414b692437
andy <andy@andydote.co.uk>
2015-07-20 20:33:20 +0100

    restExtensions: add resfulMapperFilter class

--- AutoMapper.RestExtensions.cs
+++ AutoMapper.RestExtensions.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http.Filters;
 
 namespace AutoMapper.Rest
 {
@@ -15,5 +18,39 @@ namespace AutoMapper.Rest
 	public interface IRestLinked
 	{
 		Dictionary<string, string> Links { get; set; }
+	}
+
+	public class RestfulMapperFilter : ActionFilterAttribute
+	{
+		private readonly Dictionary<Type, Type> _map;
+
+		public RestfulMapperFilter(IEnumerable<TypeMap> allTypes)
+		{
+			_map = allTypes.ToDictionary(
+				m => m.SourceType,
+				m => m.DestinationType);
+		}
+
+		public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+		{
+			var response = actionExecutedContext.Response;
+
+			object source;
+			if (response.TryGetContentValue(out source))
+			{
+				Type destinationType;
+				if (_map.TryGetValue(source.GetType(), out destinationType))
+				{
+					var destination = Mapper.Map(source, source.GetType(), destinationType);
+
+					response.Content = new ObjectContent(
+						destinationType,
+						destination,
+						((ObjectContent)(response.Content)).Formatter);
+				}
+			}
+
+			base.OnActionExecuted(actionExecutedContext);
+		}
 	}
 }
